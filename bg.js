41b00f3459762e8d4e514f29e5e28c9176429cd1
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2021-10-16 13:36:39 -0400

    Remove unused code

--- bg.js
+++ bg.js
@@ -3,28 +3,28 @@ const images = [
     './img/2.png',
     './img/3.png',
     './img/4.png',
-  ];
-  const image = new Image();
-  image.src = images[Math.floor(Math.random() * images.length)];
-  image.onload = () => {
+];
+const image = new Image();
+image.src = images[Math.floor(Math.random() * images.length)];
+image.onload = () => {
     const canvas = document.createElement('canvas');
     document.querySelector('.index').append(canvas);
     const multiply = () => {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      const count = Math.ceil(Math.random() * 15);
-      const slices = [];
-      canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
-      const width = canvas.width / count;
-      const indices = [...Array(count).keys()];
-      while (indices.length !== 0) {
-        const index = indices.splice(Math.floor(Math.random() * indices.length), 1);
-        slices.push(canvas.getContext('2d').getImageData(width * index, 0, width, canvas.height));
-      }
-      slices.forEach((slice, index) => {
-        canvas.getContext('2d').putImageData(slice, width * index, 0);
-      });
-      setTimeout(multiply, Math.random() * 60);
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        const count = Math.ceil(Math.random() * 15);
+        const slices = [];
+        canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
+        const width = canvas.width / count;
+        const indices = [...Array(count).keys()];
+        while (indices.length !== 0) {
+            const index = indices.splice(Math.floor(Math.random() * indices.length), 1);
+            slices.push(canvas.getContext('2d').getImageData(width * index, 0, width, canvas.height));
+        }
+        slices.forEach((slice, index) => {
+            canvas.getContext('2d').putImageData(slice, width * index, 0);
+        });
+        setTimeout(multiply, Math.random() * 60);
     };
     multiply();
-  };
+};
