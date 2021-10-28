18dc542fe02827412817b04f12cffee84a1e3d09
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2021-10-27 21:51:47 -0400

    Add images to array, decrease slice count

--- bg.js
+++ bg.js
@@ -3,16 +3,26 @@ const images = [
     './img/2.png',
     './img/3.png',
     './img/4.png',
+    './img/5.png',
+    './img/6.png',
+    './img/7.png',
+    './img/8.png',
+    './img/9.png',
+    './img/10.png',
+    './img/11.png',
 ];
+
 const image = new Image();
+
 image.src = images[Math.floor(Math.random() * images.length)];
+
 image.onload = () => {
     const canvas = document.createElement('canvas');
     document.querySelector('.index').append(canvas);
     const multiply = () => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        const count = Math.ceil(Math.random() * 15);
+        const count = Math.ceil(Math.random() * 8);
         const slices = [];
         canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
         const width = canvas.width / count;
@@ -25,6 +35,6 @@ image.onload = () => {
             canvas.getContext('2d').putImageData(slice, width * index, 0);
         });
         setTimeout(multiply, Math.random() * 60);
-    };
+    }
     multiply();
-};
+}
